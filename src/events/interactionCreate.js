cf591c46425bea1f0ab0f328c52fdb24e2fb1574
DashAlidev <ali89shadt@gmail.com>
2026-02-15 22:26:51 +0330

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,1 +1,18 @@
+export const name = "interactionCreate";
 
+export async function execute(interaction, client) {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction, client);
+  } catch (err) {
+    console.error(err);
+    await interaction.reply({
+      content: "❌ خطایی رخ داد!",
+      ephemeral: true
+    });
+  }
+}
