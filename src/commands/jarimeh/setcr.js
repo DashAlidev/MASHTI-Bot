8e304f943dd258047a6256ec2fb66dd550f24d53
DashAlidev <ali89shadt@gmail.com>
2026-02-15 22:33:17 +0330

    Update setcr.js

--- src/commands/jarimeh/setcr.js
+++ src/commands/jarimeh/setcr.js
@@ -1,1 +1,17 @@
+import { SlashCommandBuilder } from "discord.js";
 
+export const data = new SlashCommandBuilder()
+  .setName("setcr")
+  .setDescription("دادن دسترسی CR به کاربر")
+  .addUserOption(o =>
+    o.setName("user").setDescription("کاربر").setRequired(true)
+  );
+
+export async function execute(interaction) {
+  const user = interaction.options.getUser("user");
+  const member = interaction.guild.members.cache.get(user.id);
+
+  await member.roles.add(process.env.CR_ROLE_ID);
+
+  interaction.reply(`✔️ دسترسی CR به <@${user.id}> داده شد.`);
+}
