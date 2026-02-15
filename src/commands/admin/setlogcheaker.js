1e8309dcc2dfbb2b85066d1b06c7f5120967cb2a
DashAlidev <ali89shadt@gmail.com>
2026-02-15 22:33:58 +0330

    Update setlogcheaker.js

--- src/commands/admin/setlogcheaker.js
+++ src/commands/admin/setlogcheaker.js
@@ -1,1 +1,17 @@
+import { SlashCommandBuilder } from "discord.js";
 
+export const data = new SlashCommandBuilder()
+  .setName("setlogcheaker")
+  .setDescription("دادن دسترسی کامل Log Checker")
+  .addUserOption(o =>
+    o.setName("user").setDescription("کاربر").setRequired(true)
+  );
+
+export async function execute(interaction) {
+  const user = interaction.options.getUser("user");
+  const member = interaction.guild.members.cache.get(user.id);
+
+  await member.roles.add(process.env.LOGCHEAKER_ROLE_ID);
+
+  interaction.reply(`✔️ دسترسی Log Checker به <@${user.id}> داده شد.`);
+}
