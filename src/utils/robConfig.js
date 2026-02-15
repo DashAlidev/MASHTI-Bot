3bae3e853c57da15c80ed1aee99a65d8a1f00caa
DashAlidev <ali89shadt@gmail.com>
2026-02-15 22:27:58 +0330

    Update robConfig.js

--- src/utils/robConfig.js
+++ src/utils/robConfig.js
@@ -1,1 +1,7 @@
 
+export const robTypes = {
+  "Shop": { xp: 8, players: 2 },
+  "Mini Bank": { xp: 34, players: 2 },
+  "Jaw Shams": { xp: 50, players: 4 },
+  "Maze Bank": { xp: 90, players: 4 }
+};
