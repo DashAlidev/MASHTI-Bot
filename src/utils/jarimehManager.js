809111abf3a60aa0882db8f79ba47772948b1ef0
DashAlidev <ali89shadt@gmail.com>
2026-02-15 22:27:39 +0330

    Update jarimehManager.js

--- src/utils/jarimehManager.js
+++ src/utils/jarimehManager.js
@@ -1,1 +1,25 @@
+import { db } from "../database/db.js";
 
+export function addJarimeh(userId, amount, reason, description) {
+  const timestamp = new Date().toISOString();
+
+  db.run(
+    `
+    INSERT INTO jarimeh (userId, amount, reason, description, timestamp)
+    VALUES (?, ?, ?, ?, ?)
+  `,
+    [userId, amount, reason, description, timestamp]
+  );
+}
+
+export function getUserJarimeh(userId, callback) {
+  db.all(
+    `
+    SELECT * FROM jarimeh
+    WHERE userId = ?
+    ORDER BY id DESC
+  `,
+    [userId],
+    callback
+  );
+}
